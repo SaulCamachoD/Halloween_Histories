5e581f947dc63e5d7aa834f65fb91cba56067cfb
DianaMontoya19 <dicaro2001@hotmail.com>
2024-07-24 22:46:03 -0500

    Animaciones Skull

---
+++ Assets/Script/MoveSkull.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Serialization;
+
+public class MoveSkull : MonoBehaviour
+{
+    private Animator anim;
+    [SerializeField] private String horizontal;
+    [SerializeField] private String vertical;
+    private float speed = 10f;
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    
+    void Update()
+    {
+        float x = Input.GetAxisRaw(horizontal);
+        float y = Input.GetAxisRaw(vertical);
+        
+        transform.Translate(0, 0,- y * Time.deltaTime * speed);
+        
+        anim.SetFloat("Velx",x);
+        anim.SetFloat("Vely",y);
+
+        if (Input.GetKeyDown(KeyCode.Return))
+        {
+            anim.SetTrigger("Attack");
+        }
+    }
+}
