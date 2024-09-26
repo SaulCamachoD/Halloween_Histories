51d4ffc7600adb47f3623a3038e451911cbe948a
arco03 <dmglux13@gmail.com>
2024-09-26 16:23:22 -0500

    Update: Time script

---
+++ Assets/_Script/UI/Timer.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using Unity.PlasticSCM.Editor.WebApi;
+using UnityEngine;
+
+public class Timer : MonoBehaviour
+{
+    [SerializeField] private TextMeshProUGUI textMeshPro;
+    [SerializeField] private float timeElapse;
+    [SerializeField] private int minutes, seconds;
+
+    private float currentTime, maxTime;
+
+
+
+}
