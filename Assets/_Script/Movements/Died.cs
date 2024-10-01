4f6b84dd422623baa0112750573de9295cf552b5
DianaMontoya19 <dicaro2001@hotmail.com>
2024-09-30 22:19:32 -0500

    Juego final

--- Assets/_Script/Movements/Died.cs
+++ Assets/_Script/Movements/Died.cs
@@ -8,12 +8,17 @@ public class Died : MonoBehaviour
     [SerializeField] private PlayableDirector _timelineClip;
     [SerializeField] private LaneRunner _laneRunner;
     [SerializeField] private Timer timer;
+    [SerializeField] private Animator died;
+    [SerializeField] private Animator player;
+
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Obstaculo"))
         {
             _laneRunner.enabled = false;
             _timelineClip.enabled = true;
+            died.SetTrigger("Choque");
+            player.enabled = true;
             StartCoroutine(TimerUI());
         }
     }
